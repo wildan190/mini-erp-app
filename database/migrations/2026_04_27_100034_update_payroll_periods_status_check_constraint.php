c6dee17e8d0e0aa3a3ec309ddde0f29559dc0fd0
Wildan Belfiore <mikewildan@outlook.com>
2026-04-27 17:03:34 +0700

    fix db generate payroll periods

---
+++ database/migrations/2026_04_27_100034_update_payroll_periods_status_check_constraint.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement('ALTER TABLE payroll_periods DROP CONSTRAINT IF EXISTS payroll_periods_status_check');
+        DB::statement("ALTER TABLE payroll_periods ADD CONSTRAINT payroll_periods_status_check CHECK (status::text = ANY (ARRAY['draft'::character varying, 'processing'::character varying, 'closed'::character varying]::text[]))");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::statement('ALTER TABLE payroll_periods DROP CONSTRAINT IF EXISTS payroll_periods_status_check');
+        DB::statement("ALTER TABLE payroll_periods ADD CONSTRAINT payroll_periods_status_check CHECK (status::text = ANY (ARRAY['draft'::character varying, 'closed'::character varying]::text[]))");
+    }
+};
